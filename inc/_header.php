9f1c752efd1dfde562743ef19dad4b2734599d27
G.Franck <franckguerin05@gmail.com>
2023-04-26 17:18:46 +0200

    first commit

---
+++ inc/_header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>braizerecords</title>
+
+    <link href="https://api.fontshare.com/v2/css?f[]=clash-display@200,400,700,500,600,1,300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,36 @@
+<?php include 'inc/_header.php'; ?>
+<header class="homeheader">
+
+<div class="homeslider">
+
+<div class="slidercontent">
+<div class="slidercontent_status">Nouveau Clip</div>
+    <h3 class="slidercontent_title">LASHKA - BURN OUT</h3>
+  <!--  <h4 class="slidercontent_text">Maintenant Disponible</h4>
+    <div class="slidercontent_sociallinks">Maitenant Disponible</div>
+ <div class="slidercontent_button">Decouvrir le Clip</div>-->
+</div>
+  
+</div>
+
+
+
+</div>
+</header>
+<section class="newsslider">
+
+<!--1-->
+     <div class="newsslider_card one">
+        
+     </div>
+
+     <!--2--->
+     <div class="newsslider_card two">
+        
+        </div>
+
+        <!--3-->
+        <div class="newsslider_card three">
+        
+        </div>
+<?php include 'inc/_footer.php'; ?>
